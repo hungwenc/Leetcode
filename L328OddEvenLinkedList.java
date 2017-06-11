18d8cb4ca01b5da6b0c607372c78e118acec74eb
hhh146578 <hhh146578@gmail.com>
2017-06-11 17:20:24 +0800

    Create L328OddEvenLinkedList.java

---
+++ L328OddEvenLinkedList.java
@@ -0,0 +1,62 @@
+/*L328. Odd Even Linked List
+Given a singly linked list, group all odd nodes together followed by the even nodes. 
+Please note here we are talking about the node number and not the value in the nodes.
+You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+Example:
+Given 1->2->3->4->5->NULL,
+return 1->3->5->2->4->NULL.
+Note:
+The relative order inside both the even and odd groups should remain as it was in the input. 
+The first node is considered odd, the second node even and so on ...
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+//time O(n)
+//space O(1)
+//better version 
+public class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null || head.next == null || head.next.next == null) return head;
+        ListNode odd = head;
+        ListNode even_head = head.next;
+        ListNode even = head.next;
+        while(odd.next!=null && even.next!=null){
+            odd.next = even.next;
+            even.next = odd.next.next;
+            even = even.next;
+            odd = odd.next;
+        }
+        odd.next = even_head;
+        return head;
+    }
+}
+
+// 一開始寫的
+// public class Solution {
+//     public ListNode oddEvenList(ListNode head) {
+//         if(head == null || head.next == null || head.next.next == null) return head;
+//         ListNode odd = head;
+//         ListNode even_head = head.next;
+//         ListNode even = head.next;
+//         while(odd!=null && even!=null){
+//             odd.next = even.next;
+//             if(odd.next == null){
+//                 even.next = null;
+//             }else{
+//                 even.next = odd.next.next;
+//             }
+//             even = even.next;
+//             if(odd.next != null){
+//                 odd = odd.next;
+//             }
+//         }
+//         odd.next = even_head;
+//         return head;
+//     }
+// }
