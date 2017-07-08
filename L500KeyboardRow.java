8d25442cefcb86d730dfdc05fb03376a70f28ec0
hhh146578 <hhh146578@gmail.com>
2017-07-09 02:34:40 +0800

    Update L500KeyboardRow.java

--- L500KeyboardRow.java
+++ L500KeyboardRow.java
@@ -33,3 +33,10 @@ public class Solution {
         return res.toArray(new String[0]);
     }
 }
+
+// sol2 Regular Expression
+/*
+public String[] findWords(String[] words) {
+    return Stream.of(words).filter(s -> s.toLowerCase().matches("[qwertyuiop]*|[asdfghjkl]*|[zxcvbnm]*")).toArray(String[]::new);
+}
+*/
