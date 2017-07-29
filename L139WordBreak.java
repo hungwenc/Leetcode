80b191224761f875fb127a398461b0cdc4702c9c
hhh146578 <hhh146578@gmail.com>
2017-07-29 10:09:46 +0800

    Update L139WordBreak.java

--- L139WordBreak.java
+++ L139WordBreak.java
@@ -12,10 +12,10 @@ The wordDict parameter had been changed to a list of strings (instead of a set o
 */
 
 // n = s.length()
-//time comp O(n^2)???? but somebody said it's not O(n^2), but O(n*wlen).
+//time comp O(n^2)
 //space comp O(n)
 //http://www.programcreek.com/2012/12/leetcode-solution-word-break/
-
+//問true or false, 此題是dp 題 當前狀態由之前狀態來決定 
 public class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         boolean[] match = new boolean[s.length() + 1];  // record past 
