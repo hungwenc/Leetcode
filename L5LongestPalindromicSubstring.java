9496f6a72436f92a9208658b9e7ce24859531417
hhh146578 <hhh146578@gmail.com>
2017-08-25 12:33:59 +0800

    Update L5LongestPalindromicSubstring.java

--- L5LongestPalindromicSubstring.java
+++ L5LongestPalindromicSubstring.java
@@ -9,10 +9,40 @@ Input: "cbbd"
 Output: "bb"
 */
 
-// sol1 dynamic progrmming
+// sol1 simple solution 直觀
+// time O(n^2)
+// space O(1)
+class Solution {
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if(s == null || len < 2) return s;
+        if(len > 1000) return null;
+        String ans = s.substring(0, 1);
+        for(int i = 0; i < len; i++){
+            // odd case: expand from the center i
+            String temp = expand(s, i, i);
+            if(temp.length() > ans.length()) ans = temp;
+            // even case: expand from the center i and i+1
+            temp = expand(s, i, i+1);
+            if(temp.length() > ans.length()) ans = temp;       
+        }
+        return ans;
+    }
+    private String expand(String s, int start, int end){
+        while(start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)){
+            start--;
+            end++;
+        }
+        // when the start break out of loop start might be "-1", so substring should be 'start + 1' to 'end'
+        return s.substring(start + 1, end);
+    }
+}
+
+// sol2 dynamic progrmming
 // time O(n^2)
 // space O(n^2)
 // analysis: https://leetcode.com/problems/longest-palindromic-substring/solution/
+/*
 class Solution {
     public String longestPalindrome(String s) {
         int len = s.length();
@@ -42,36 +72,6 @@ class Solution {
             }    
         }
         return s.substring(ans_left, ans_right + 1);
-    }
-}
-
-// sol2 simple solution 直觀
-// time O(n^2)
-// space O(1)
-/*
-class Solution {
-    public String longestPalindrome(String s) {
-        int len = s.length();
-        if(s == null || len < 2) return s;
-        if(len > 1000) return null;
-        String ans = s.substring(0, 1);
-        for(int i = 0; i < len; i++){
-            // odd case: expand from the center i
-            String temp = expand(s, i, i);
-            if(temp.length() > ans.length()) ans = temp;
-            // even case: expand from the center i and i+1
-            temp = expand(s, i, i+1);
-            if(temp.length() > ans.length()) ans = temp;       
-        }
-        return ans;
-    }
-    private String expand(String s, int start, int end){
-        while(start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)){
-            start--;
-            end++;
-        }
-        // when the start break out of loop start might be "-1", so substring should be 'start + 1' to 'end'
-        return s.substring(start + 1, end);
     }
 }
 */
