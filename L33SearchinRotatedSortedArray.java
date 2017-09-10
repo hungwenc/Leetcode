3c913567dcea9745ad096a7fa5a7bcc2b10f06f0
hhh146578 <hhh146578@gmail.com>
2017-09-10 12:05:24 -0400

    Update L33SearchinRotatedSortedArray.java

--- L33SearchinRotatedSortedArray.java
+++ L33SearchinRotatedSortedArray.java
@@ -6,6 +6,44 @@ You may assume no duplicate exists in the array.
 */
 // idea: Binary search tree, find pattern
 // http://fisherlei.blogspot.tw/2013/01/leetcode-search-in-rotated-sorted-array.html
+
+// sol1
+class Solution {
+    public int search(int[] nums, int target) {
+        if(nums == null || nums.length == 0) return -1;
+        int left = 0;
+        int right = nums.length-1;
+        int mid;
+        while(left + 1 < right){ // 相鄰
+            mid = left + (right - left)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            // care about nums[mid] == nums[left]
+            if(nums[mid] >= nums[left]){ //  no need to write && nums[left] > nums[right]
+                // left hand side sorted
+                if(target <= nums[mid] && nums[left] <= target){
+                    right = mid;
+                }else{
+                    left = mid;
+                }
+            }else { // no need to write && nums[right] < nums[left]
+                // right hand side sorted
+                if(target <= nums[right] && target >= nums[mid]){
+                    left = mid;
+                }else{
+                    right = mid;
+                }
+            }
+        }
+        if(nums[left] == target) return left;
+        if(nums[right] == target) return right;
+        return -1;
+    }
+}
+
+// sol2 my solution
+/*
 class Solution {
     public int search(int[] nums, int target) {
         if(nums == null || nums.length == 0) return -1;
@@ -37,3 +75,4 @@ class Solution {
         return -1;
     }
 }
+*/
