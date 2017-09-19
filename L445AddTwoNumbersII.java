cef555e22e1323c1bffec8ced6c03322c3de5b1a
hhh146578 <hhh146578@gmail.com>
2017-09-19 01:22:17 -0400

    Update L445AddTwoNumbersII.java

--- L445AddTwoNumbersII.java
+++ L445AddTwoNumbersII.java
@@ -5,7 +5,6 @@ Add the two numbers and return it as a linked list.
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 Follow up:
 What if you cannot modify the input lists? In other words, reversing the lists is not allowed.
-
 Example:
 Input: (7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 8 -> 0 -> 7
@@ -23,24 +22,29 @@ Output: 7 -> 8 -> 0 -> 7
 if number of L1: n &　L2: m (n is larger than m)
 time compl: O(n)
 space compl: O(n)
-Runtime: 120 ms / beat 2.38 % of java submissions.
+Runtime: 77  ms / beat 20.01 % of java submissions.
+// 7243 + 564 = 7807
+// 要先製造出加法右側對齊 -> 用stack來達成
 */
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         Stack<Integer> s1 = new Stack<Integer>();
         Stack<Integer> s2 = new Stack<Integer>();
         //O(n)
-        while(l1!=null || l2!=null){
-            if(l1!=null){
-                s1.push(l1.val);
-                l1 = l1.next;
-            }
-            if(l2!=null){
-                s2.push(l2.val);
-                l2 = l2.next;
-            }
+        while(l1!=null && l2!=null){
+            s1.push(l1.val);
+            l1 = l1.next;
+            s2.push(l2.val);
+            l2 = l2.next;
         }
-        
+        while(l1!=null){
+            s1.push(l1.val);
+            l1 = l1.next;
+        }
+        while(l2!=null){
+            s2.push(l2.val);
+            l2 = l2.next;
+        }
         ListNode dummy = new ListNode(-1);
         ListNode cur = dummy;
         ListNode temp = null;
@@ -50,18 +54,20 @@ public class Solution {
         int carry = 0;
         
         // O(n)
-        while(!s1.isEmpty() || !s2.isEmpty()){
-            if(!s1.isEmpty()){
-                val1 = s1.pop();
-            }else{
-                val1 = 0;
-            }
-            if(!s2.isEmpty()){
-                val2 = s2.pop();
-            }else{
-                val2 = 0;
-            }
+        while(!s1.isEmpty() && !s2.isEmpty()){
+            val1 = s1.pop();
+            val2 = s2.pop();
             sum = val1 + val2 + carry;
+            carry = sum / 10;
+            sum = sum % 10;
+            // insert to the front of Linked List
+            temp = new ListNode(sum);
+            temp.next = cur.next;
+            cur.next = temp;
+        }
+        while(!s1.isEmpty()){
+            val1 = s1.pop();
+            sum = val1 + carry;
             carry = sum / 10;
             sum = sum % 10;
             
@@ -70,7 +76,17 @@ public class Solution {
             temp.next = cur.next;
             cur.next = temp;
         }
-        
+        while(!s2.isEmpty()){
+            val2 = s2.pop();
+            sum = val2 + carry;
+            carry = sum / 10;
+            sum = sum % 10;
+            
+            // insert to the front of Linked List
+            temp = new ListNode(sum);
+            temp.next = cur.next;
+            cur.next = temp;
+        }
         
         if(carry == 1){
             temp = new ListNode(carry);
