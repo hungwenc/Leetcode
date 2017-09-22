71b113cc1963c15b30d57977ee58ca181dc04c45
hhh146578 <hhh146578@gmail.com>
2017-09-21 20:33:22 -0400

    Update L167TwoSumII-Inputarrayissorted.java

--- L167TwoSumII-Inputarrayissorted.java
+++ L167TwoSumII-Inputarrayissorted.java
@@ -1,13 +1,24 @@
-/* L167. Two Sum II - Input array is sorted
+/*L167. Two Sum II - Input array is sorted
 Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+
 The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+
 You may assume that each input would have exactly one solution and you may not use the same element twice.
+
 Input: numbers={2, 7, 11, 15}, target=9
 Output: index1=1, index2=2
 */
+/*We can apply Two Sum's solutions directly to 
+Sol1 brute force: time: O(n^2), space O(1) 
+Sol2 hash table: time O(n), space O(n)
+However, both existing solutions do not make use of the property where the input array is sorted. We can do better.
+*/
+//sol3 best solution, two pointers
+//time O(n)
+//space O(1)
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        //  already sorted in ascending order
+        // already sorted in ascending order
         // exactly one solution and you may not use the same element twice.
         int head = 0;
         int tail = numbers.length-1;
