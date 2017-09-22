65caf83a643ded15b0e36f03e485dc0424084141
hhh146578 <hhh146578@gmail.com>
2017-09-21 23:10:56 -0400

    Update L15. 3Sum.java

--- L15. 3Sum.java
+++ L15. 3Sum.java
@@ -8,52 +8,61 @@ A solution set is:
   [-1, -1, 2]
 ]
 */
+
+// idea https://www.youtube.com/watch?v=gq-uWp327m8&t=358s
+// sol: use three pointers( base pointer, left pointer, right pointer)
 //time complexity O(N^2)
 //space complexity O(1)
+// * key part: avoid duplicate solution
 public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         //sort the array first
-        Arrays.sort(nums);
+        Arrays.sort(nums); // time: O(log n)
         List<List<Integer>> outer = new LinkedList<List<Integer>>();
         if(nums.length < 3) return outer;
         // be careful that we might have repeated value (not distinct value)
         // skip equal elements to avoid duplicates in the answer 
-        for(int i = 0; i < nums.length ; i++){
+        int base = 0;
+        while(base < nums.length - 2){ // base pointer
             // two pointers
-            int j = i + 1;
-            int k = nums.length -1;
+            int left = base + 1; // left pointer
+            int right = nums.length -1; // right pointer
             
             // avoid counting the repeated 1st element
-            if(i == 0 || (i > 0 && (nums[i]!=nums[i-1]))){
-                while(j < k){
-                    int sum = (nums[j] + nums[k] + nums[i]);
+            if(base == 0 || (base > 0 && (nums[base]!=nums[base-1]))){
+                while(left < right){
+                    int sum = (nums[base] + nums[left] + nums[right]);
                     if(sum == 0){
                         List<Integer> inner = new LinkedList<Integer>();
-                        inner.add(nums[i]);
-                        inner.add(nums[j]);
-                        inner.add(nums[k]);
+                        inner.add(nums[base]);
+                        inner.add(nums[left]);
+                        inner.add(nums[right]);
                         outer.add(inner);
                         // avoid counting the repeated 2nd element
-                        while(j < k && nums[j+1] == nums[j]){
-                            j++;
-                        }
-                        j = j + 1;
-                        
+                        left = moveRight(nums, left + 1);
                         // avoid counting the repeated 3rd element
-                        while(j < k && nums[k-1] == nums[k]){
-                            k--;
-                        }
-                        k = k - 1;
-                    }
-                    if(sum < 0){
-                        j++;
-                    }
-                    if(sum > 0){
-                        k--;
+                        right = moveLeft(nums, right - 1);
+                    }else if(sum > 0){
+                        right = moveLeft(nums, right -1);
+                    }else if(sum < 0){
+                        left = moveRight(nums, left + 1);
                     }   
                 }
             }
+            base = moveRight(nums, base + 1);
         }
         return outer;
+    }
+    public int moveRight(int nums[], int left){
+        while(left == 0 || (left < nums.length && nums[left] == nums[left - 1])){
+            left++;
+        }    
+        return left;
+    }
+    public int moveLeft(int nums[], int right){
+        while(right == nums.length || (right >= 0 && nums[right] == nums[right + 1])){
+            right--;
+        }
+        return right;
     }
 }
