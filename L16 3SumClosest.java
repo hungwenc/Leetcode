0a778db1a6fc078f0ca5cefdf96fb1d465992d89
hhh146578 <hhh146578@gmail.com>
2017-09-21 23:19:25 -0400

    Update L16 3SumClosest.java

--- L16 3SumClosest.java
+++ L16 3SumClosest.java
@@ -1,16 +1,19 @@
 /*L16. 3Sum Closest
 Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
-For example, given array S = {-1 2 1 -4}, and target = 1.
-The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
+    For example, given array S = {-1 2 1 -4}, and target = 1.
+
+    The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
 */
-//time: O(N^2)
-//space: O(1)
+//先sort 再用three pointers
+//time O(n^2)
+//space O(1)	
 public class Solution {
     public int threeSumClosest(int[] nums, int target) {
         int min_diff = Integer.MAX_VALUE;
         int diff = 0;
         int sum = 0;
-        Arrays.sort(nums);
+        Arrays.sort(nums); // time: O(nlogn)
         for(int i = 0; i<nums.length; i++){
             int j = i + 1;
             int k = nums.length-1;
