b6f226d382ad0e459c62fe0cebb564328ea1be79
hhh146578 <hhh146578@gmail.com>
2017-09-22 11:24:37 -0400

    Update L198HouseRobber.java

--- L198HouseRobber.java
+++ L198HouseRobber.java
@@ -27,6 +27,7 @@ public class Solution {
         int[] dp = new int[2]; // we only need size of 2 to record 
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
+        
         for(int i = 2; i < nums.length; i++){ 
             dp[i%2] = Math.max(nums[i] + dp[(i-2)%2], dp[(i-1)%2]); // use % 
         }
@@ -50,6 +51,23 @@ public class Solution {
             dp[i] = Math.max(nums[i] + dp[i-2], dp[i-1]);
         }
         return dp[nums.length - 1];
+    }
+}
+*/
+
+// sol3
+// idea:  https://www.youtube.com/watch?v=-i2BFAU25Zk
+/*
+public class Solution{
+    public int rob(int[] nums){
+        int rob = 0;
+        int notRob = 0;
+        for(int num: nums){
+            int pre = Math.max(notRob, rob);
+            rob = notRob + num;
+            notRob = pre;
+        }
+        return Math.max(rob, notRob);
     }
 }
 */
