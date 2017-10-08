51096b26918b04c7e05c359a61a905b18184185d
hhh146578 <hhh146578@gmail.com>
2017-10-07 23:30:57 -0400

    Update L110BalancedBinaryTree.java

--- L110BalancedBinaryTree.java
+++ L110BalancedBinaryTree.java
@@ -1,29 +1,15 @@
-import java.util.*;
-/* L 110. Balanced Binary Tree
- * Given a binary tree, determine if it is height-balanced.
+/**L110. Balanced Binary Tree
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 
-For this problem, a height-balanced binary tree is defined as a binary tree 
-in which the depth of the two subtrees of every node never differ by more than 1.
-*/
-
-//sol1
-//利用-1當signal表示不balanced
-//在計算tree高度的時候同時考慮看此node是不是balanced
-//Time complexity: O(n)
-
-//sol2
-//依照定義的解法應該會是 Time complexity: O(n^2)
-
-public class L110BalancedBinaryTree {
-	/**
-	 * Definition for a binary tree node.
-	 * public class TreeNode {
-	 *     int val;
-	 *     TreeNode left;
-	 *     TreeNode right;
-	 *     TreeNode(int x) { val = x; }
-	 * }
-	 */
+// video: https://www.youtube.com/watch?v=Be5CUodZliM
+public class Solution {
     public int maxDepth(TreeNode root) {
         if(root == null){
             return 0;
