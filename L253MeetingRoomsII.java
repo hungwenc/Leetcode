dc928c2d2ba363391fdbfb449f9e6ffe7ad96cff
hhh146578 <hhh146578@gmail.com>
2017-10-23 22:32:04 -0400

    Update L253MeetingRoomsII.java

--- L253MeetingRoomsII.java
+++ L253MeetingRoomsII.java
@@ -44,31 +44,44 @@ class Solution {
 
 //sol2
 /*
-public int minMeetingRooms(Interval[] intervals) {
-    if(intervals==null||intervals.length==0)
-        return 0;
- 
-    Arrays.sort(intervals, new Comparator<Interval>(){
-        public int compare(Interval i1, Interval i2){
-            return i1.start-i2.start;
+Just want to share another idea that uses min heap (default priority queue is min heap), 
+average time complexity is O(nlogn).
+class Solution {
+    public int minMeetingRooms(Interval[] intervals) {
+        if (intervals == null || intervals.length == 0)
+            return 0;
+
+        // Sort the intervals by start time
+        Arrays.sort(intervals, new Comparator<Interval>() {
+            public int compare(Interval a, Interval b) { return a.start - b.start; }
+        });
+
+        // Use a min heap to track the minimum end time of merged intervals
+        PriorityQueue<Interval> heap = new PriorityQueue<Interval>(intervals.length, new Comparator<Interval>() {
+            public int compare(Interval a, Interval b) { return a.end - b.end; }
+        });
+
+        // start with the first meeting, put it to a meeting room
+        heap.offer(intervals[0]);
+
+        for (int i = 1; i < intervals.length; i++) {
+            // get the meeting room that finishes earliest
+            Interval interval = heap.poll();
+
+            if (intervals[i].start >= interval.end) {
+                // if the current meeting starts right after 
+                // there's no need for a new room, merge the interval
+                interval.end = intervals[i].end;
+            } else {
+                // otherwise, this meeting needs a new room
+                heap.offer(intervals[i]);
+            }
+
+            // don't forget to put the meeting room back
+            heap.offer(interval);
         }
-    });
- 
-    PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
-    int count=1;
-    queue.offer(intervals[0].end);
- 
-    for(int i=1; i<intervals.length; i++){
-        if(intervals[i].start<queue.peek()){
-            count++;
- 
-        }else{
-            queue.poll();
-        }
- 
-        queue.offer(intervals[i].end);
+
+        return heap.size();
     }
- 
-    return count;
 }
 */
