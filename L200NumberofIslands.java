ccef47791a1e0f54a13c6f3a30efc87285f62eee
hhh146578 <hhh146578@gmail.com>
2017-10-29 13:30:03 -0400

    Update L200NumberofIslands.java

--- L200NumberofIslands.java
+++ L200NumberofIslands.java
@@ -18,6 +18,45 @@ Example 2:
 Answer: 3
 */
 // dfs + connected component
+
+//time O(m*n)
+//space O(m*n) // worst case call stack
+// 不使用boolean[][] visited, 直接把visited 過的grid 設成 '0'
+public class Solution {
+    public int numIslands(char[][] grid) {
+        if(grid == null || grid.length == 0 ||  grid[0] == null || grid[0].length == 0) return 0;
+        int row = grid.length;
+        int col = grid[0].length;
+        int count = 0;
+        // boolean[][] visited = new boolean[row][col];
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(grid[i][j] == '1'){
+                    dfs(grid, i, j);
+                    count++; 
+                }
+            }
+        }         
+        return count;
+    }
+    private void dfs(char[][] grid, int i, int j){
+        if(i < 0 || i >= grid.length) return;
+        if(j < 0 || j >= grid[0].length) return;
+        if(grid[i][j] == '1'){
+            grid[i][j] = '0';
+            dfs(grid, i+1, j);
+            dfs(grid, i-1, j);
+            dfs(grid, i, j+1);
+            dfs(grid, i, j-1);
+        }else{
+            return;
+        }
+        return;
+    }
+}
+
+
+/*
 public class Solution {
     public int numIslands(char[][] grid) {
         if(grid == null || grid.length == 0 ||  grid[0] == null || grid[0].length == 0) return 0;
@@ -50,3 +89,4 @@ public class Solution {
         return;
     }
 }
+*/
