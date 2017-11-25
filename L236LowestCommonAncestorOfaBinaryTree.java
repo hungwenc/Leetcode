7769a7912d2fc967ebbe5cff4be9ca5674afee2e
hhh146578 <hhh146578@gmail.com>
2017-11-24 23:48:38 -0500

    Update L236LowestCommonAncestorOfaBinaryTree.java

--- L236LowestCommonAncestorOfaBinaryTree.java
+++ L236LowestCommonAncestorOfaBinaryTree.java
@@ -51,3 +51,36 @@ public class Solution {
         return right; // left==null but right!=null
     }
 }
+
+/*from lintcode
+// divide and conquer
+// 在root为根的二叉树中找A,B的LCA:
+// 如果找到了就返回这个LCA
+// 如果只碰到A，就返回A
+// 如果只碰到B，就返回B
+// 如果都没有，就返回null
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || p == null || q == null){
+            return null;    
+        } 
+        
+        if (root == p || root == q){
+          return root;//p q其中一個就是現在這個root
+        }
+        
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+        if (left != null){
+            return left;
+        }
+        if (right != null){
+            return right;
+        }
+        return null;
+    }
+}
+*/
