dec5ff5c1b58e3d096750217d9129e6b78438fab
hhh146578 <hhh146578@gmail.com>
2017-12-22 12:37:28 -0500

    Update L141LinkedListCycle.java

--- L141LinkedListCycle.java
+++ L141LinkedListCycle.java
@@ -1,22 +1,33 @@
-/**
- * 141. Linked List Cycle
+/*L141. Linked List Cycle
 Given a linked list, determine if it has a cycle in it.
-
 Follow up:
 Can you solve it without using extra space?
+*/
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
  */
-
-public class L141LinkedListCycle {
+public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head == null || head.next == null) return false;
-        ListNode slow = head;
+        if (head == null || head.next == null) {
+            return false;
+        }
         ListNode fast = head.next;
-        while(slow != fast){
-            if(fast == null || fast.next==null){
+        ListNode slow = head;
+        while(slow != fast) {
+            if (fast == null || fast.next == null) { 
+                //要判斷fast.next是否為null 不然fast.next.next會有問題
                 return false;
             }
-            slow = slow.next;
-            fast = fast.next.next;
+            fast = fast.next.next; //一次跳兩格
+            slow = slow.next; //一次跳一格
         }
         return true;
     }
