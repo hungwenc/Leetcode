db40de40183ae20653f133ef89d1b5053dc5548a
hhh146578 <hhh146578@gmail.com>
2017-12-26 16:37:58 -0500

    Update L283MoveZeroes.java

--- L283MoveZeroes.java
+++ L283MoveZeroes.java
@@ -1,28 +1,30 @@
-/*
-283. Move Zeroes
+/*L283. Move Zeroes
 Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
 For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
-
 Note:
 You must do this in-place without making a copy of the array.
 Minimize the total number of operations.
 */
-
-public class L283MoveZeroes {
+//sol 同方向 雙指針
+public class Solution {
     public void moveZeroes(int[] nums) {
-        for(int i=0; i<nums.length-1; i++){
-            if(nums[i]==0){
-                for(int j=i+1; j<nums.length; j++){
-                    if(nums[j]!=0){
-                        int temp;
-                        temp = nums[i];
-                        nums[i] = nums[j];
-                        nums[j] = temp;
-                        i++;
-                    }
-                }   
-            }
+        if (nums == null || nums.length == 0) {
+            return;
         }
+        int slow = 0; // slow 的左側 都是非零的數
+        int fast = 0; // 用 fast 去找非零的數 再跟slow交換
+        while (fast < nums.length) {
+            if (nums[fast] != 0) {
+                swap(nums, slow, fast);
+                slow++;
+            }
+            fast++;
+        }
+    }
+    private void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
     }
 }
