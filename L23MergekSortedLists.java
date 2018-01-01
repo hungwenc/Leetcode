88a1286d37467ba2bd032779aa120f9cd3790bb3
hhh146578 <hhh146578@gmail.com>
2018-01-01 10:05:35 -0500

    Update L23MergekSortedLists.java

--- L23MergekSortedLists.java
+++ L23MergekSortedLists.java
@@ -15,7 +15,144 @@
 //http://www.geeksforgeeks.org/merge-k-sorted-linked-lists/
 
 //http://www.jiuzhang.com/solutions/merge-k-sorted-lists/
-//sol1. min heap priority queue
+
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */
+//三種解法都要寫寫看
+// sol1 heap priorityQueue 最容易寫解法 time complexity = O(N logK)
+public class Solution {
+    private Comparator<ListNode> ListNodeComparator = new Comparator<ListNode>(){
+        public int compare(ListNode l1, ListNode l2) {
+            return l1.val - l2.val;
+        }
+    };
+    public ListNode mergeKLists(List<ListNode> lists) {  
+        if (lists == null || lists.size() == 0) {
+            return null;
+        }
+        
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
+        Queue<ListNode> pq = new PriorityQueue<ListNode>(lists.size(), ListNodeComparator);
+        
+        for (int i = 0 ; i < lists.size(); i++) {
+            ListNode node = lists.get(i);
+            if (node != null) {
+                pq.add(node);
+            }
+        }
+
+        while (!pq.isEmpty()){ // O(N) N 為總共的node的數
+            ListNode minNode = pq.poll(); // pq 的 poll time complextity O(logK)
+            tail.next = minNode;
+            tail = tail.next;
+            if (minNode.next != null) {
+                pq.add(minNode.next); // pq 的 add time complextity O(logK)
+            }
+        }
+        return dummy.next;
+    }
+}
+
+// sol2 merge two by two 解法 bottom -> up, time complexity = O(N logK)
+/*
+public class Solution {
+    public ListNode mergeKLists(List<ListNode> lists) {  
+        if (lists == null || lists.size() == 0) {
+            return null;
+        }
+        while (lists.size() > 1) {  // 表示可以繼續的merge
+            List<ListNode> newList = new ArrayList<>();
+            for (int i = 0; i + 1 < lists.size(); i = i + 2) { // 這裡bound 應該要 i+1 < size
+                ListNode mergedList = mergeTwoLists(lists.get(i), lists.get(i + 1));
+                newList.add(mergedList);
+            }
+            if (lists.size() % 2 == 1) {
+                newList.add(lists.get(lists.size() - 1));
+            }
+            lists = newList;
+        }
+        return lists.get(0);
+    }
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode lastNode = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                lastNode.next = l1;
+                l1 = l1.next;
+            } else {
+                lastNode.next = l2;
+                l2 = l2.next;
+            }
+            lastNode = lastNode.next;
+        }
+        
+        if (l1 != null) {
+            lastNode.next = l1;
+        }
+        if (l2 != null) {
+            lastNode.next = l2;
+        }
+        return dummy.next;
+    }
+}
+*/
+
+// sol3 Divide & Conquer 解法 top -> down,(很像mergeSort寫法) time complexity = O(N logK)
+/*
+public class Solution {
+    public ListNode mergeKLists(List<ListNode> lists) {  
+        if (lists == null || lists.size() == 0) {
+            return null;
+        }
+
+        return mergeHelper(lists, 0, lists.size() - 1);
+    }
+    public ListNode mergeHelper(List<ListNode> lists, int start, int end) {
+        if (start >= end) {
+            return lists.get(start);
+        }
+        int mid = start + (end - start) / 2;
+        ListNode left = mergeHelper(lists, start, mid);
+        ListNode right = mergeHelper(lists, mid + 1, end);
+        return mergeTwoLists(left, right);
+    }
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode lastNode = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                lastNode.next = l1;
+                l1 = l1.next;
+            } else {
+                lastNode.next = l2;
+                l2 = l2.next;
+            }
+            lastNode = lastNode.next;
+        }
+        
+        if (l1 != null) {
+            lastNode.next = l1;
+        }
+        if (l2 != null) {
+            lastNode.next = l2;
+        }
+        return dummy.next;
+    }
+}
+*/
+/*
+//min heap priority queue
 public class Solution {
     
     // implement comparator
@@ -48,97 +185,6 @@ public class Solution {
                 pq.offer(temp.next);
             }
         }
-        return dummy.next;
-    }
-}
-
-/*
-// sol 2: Divide & Conquer
-public class Solution {
-    public ListNode mergeKLists(List<ListNode> lists) {
-        if (lists.size() == 0) {
-            return null;
-        }
-        return mergeHelper(lists, 0, lists.size() - 1);
-    }
-    
-    private ListNode mergeHelper(List<ListNode> lists, int start, int end) {
-        if (start == end) {
-            return lists.get(start);
-        }
-        
-        int mid = start + (end - start) / 2;
-        ListNode left = mergeHelper(lists, start, mid);
-        ListNode right = mergeHelper(lists, mid + 1, end);
-        return mergeTwoLists(left, right);
-    }
-    
-    private ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
-        while (list1 != null && list2 != null) {
-            if (list1.val < list2.val) {
-                tail.next = list1;
-                tail = list1;
-                list1 = list1.next;
-            } else {
-                tail.next = list2;
-                tail = list2;
-                list2 = list2.next;
-            }
-        }
-        if (list1 != null) {
-            tail.next = list1;
-        } else {
-            tail.next = list2;
-        }
-        
-        return dummy.next;
-    }
-}
-
-// sol 3: merge two by two
-public class Solution {
-    public ListNode mergeKLists(List<ListNode> lists) {  
-        if (lists == null || lists.size() == 0) {
-            return null;
-        }
-        
-        while (lists.size() > 1) {
-            List<ListNode> new_lists = new ArrayList<ListNode>();
-            for (int i = 0; i + 1 < lists.size(); i += 2) {
-                ListNode merged_list = merge(lists.get(i), lists.get(i+1));
-                new_lists.add(merged_list);
-            }
-            if (lists.size() % 2 == 1) {
-                new_lists.add(lists.get(lists.size() - 1));
-            }
-            lists = new_lists;
-        }
-        
-        return lists.get(0);
-    }
-    
-    private ListNode merge(ListNode a, ListNode b) {
-        ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
-        while (a != null && b != null) {
-            if (a.val < b.val) {
-                tail.next = a;
-                a = a.next;
-            } else {
-                tail.next = b;
-                b = b.next;
-            }
-            tail = tail.next;
-        }
-        
-        if (a != null) {
-            tail.next = a;
-        } else {
-            tail.next = b;
-        }
-        
         return dummy.next;
     }
 }
