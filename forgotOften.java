41a6080f26551d75947563c30bfb82a389c7f85b
hhh146578 <hhh146578@gmail.com>
2018-01-01 15:28:20 -0500

    Update forgotOften.java

--- forgotOften.java
+++ forgotOften.java
@@ -32,7 +32,7 @@ public List<Integer> topk() {
     while (it.hasNext()) {
         result.add((Integer) it.next());
     }
-    Collections.sort(result, Collections.reverseOrder()); // 從大到小排列 或用Collections.reverse(list)
+    Collections.sort(result, Collections.reverseOrder()); // 從大到小排列   其他常用的Collections.reverse(list)
     return result;
 }
 
