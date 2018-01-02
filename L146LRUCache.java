308c476c7c6f6e275ef194e16e548a9ade15b8f6
hhh146578 <hhh146578@gmail.com>
2018-01-02 18:34:01 -0500

    Update L146LRUCache.java

--- L146LRUCache.java
+++ L146LRUCache.java
@@ -116,3 +116,101 @@ public class LRUCache {
  * int param_1 = obj.get(key);
  * obj.put(key,value);
  */
+
+/*Lintcode 134. LRU Cache
+Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and set.
+
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+set(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+*/
+/*
+首先，对于cache，如果希望有O(1)的查找复杂度，肯定要用hashmap来保存key和对象的映射。
+对于LRU而言，问题在于如何用O(1)解决cache entry的替换问题。
+
+cache的存储是一个链表的话，那么只要保证从头到尾的顺序就是cache从舊到新的顺序就好了，对于任何一个節點，如果被訪問了，那么就將該節點移至尾部。如果cache已满，那么就把頭部的删掉，从尾部插入新節點。
+
+所以，需要用到两个数据结构
+
+1. hashmap，保存key和对象(node)記憶體位置的映射。利用hashMap來達到O(1)的get
+
+2. double linked list，保存對象新舊程度的序列。利用雙向linkedlist(一開始想到是queue但還需要處理拿queue
+中間的元素時要怎麼移動的問題 所以使用linked list)來達成LRU的evict操作
+*/
+public class LRUCache {
+    // 自己定義Node的class
+    class Node {
+        int key;
+        int value;
+        Node prev;
+        Node next;
+        private Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+            prev = null;
+            next = null;
+        }
+    }
+    /*
+     * @param capacity: An integer
+     */
+    int capacity;
+    Map<Integer, Node> map = new HashMap<>();
+    Node head = new Node(-1, -1);
+    Node tail = new Node(-1, -1);
+    
+    public LRUCache(int capacity) {
+        // do intialization if necessary
+        this.capacity = capacity;
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+        Node curr = map.get(key); // obtain this Node from map  
+	  
+	    // remove curr Node position from the double linked list
+        curr.prev.next = curr.next;
+        curr.next.prev = curr.prev;
+        
+        // update this Node position
+        move_to_tail(curr);
+        return curr.value;
+    }
+
+    /*
+     * @param key: An integer
+     * @param value: An integer
+     * @return: nothing
+     */
+    public void set(int key, int value) {
+        // 呼叫get 函數 會順便幫我們move to tail! 在這不要用map.containsKey 
+        if (get(key) != -1) {
+            map.get(key).value = value;
+            return;
+        }
+        Node insert = new Node(key, value);
+        if (map.size() == capacity) { // cache size 滿了 要 evict
+            Node evict = head.next;
+            map.remove(evict.key);
+            head.next = evict.next;
+            evict.next.prev = head;
+        }
+        map.put(key, insert);
+        move_to_tail(insert);
+        return;
+    }
+    
+    public void move_to_tail(Node curr){
+        // 把curr node 插入 tail 和 tail.prev 這兩個node中間
+        // 先把curr node 和兩節點新增pointer 
+        curr.prev = tail.prev;
+        curr.next = tail;
+        // 再把舊友的 pointer 砍斷
+        curr.prev.next = curr;
+        curr.next.prev = curr;
+    }
+}
+
